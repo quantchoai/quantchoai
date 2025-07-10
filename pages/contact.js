13026c6c56162a658aa7ea0ea0cc7a51ce64d97d
quantchoai <scho@quantchoai.com>
2025-07-10 03:45:55 -0700

    Add files via upload

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Contact Us</h1>
-      <p>Email us at: <a href="mailto:info@quantcho.com">info@quantcho.com</a></p>
+      <p>Email us at: <a href="mailto:info@quantchoai.com">info@quantchoai.com</a></p>
     </div>
   );
 }
